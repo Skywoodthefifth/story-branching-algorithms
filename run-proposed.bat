e470706422d77d76dc14918f480909e301ead434
Vo Huu Nam Truong <namtruong0900@gmail.com>
2025-02-15 12:47:29 +0700

    + update bat

--- run-proposed.bat
+++ run-proposed.bat
@@ -6,12 +6,16 @@ call .venv\Scripts\activate
 REM Run the Python script
 python main.py generate-story-with ^
     --approach proposed ^
+    --game-genre "japanese visual novel"
     --num-chapters 3 ^
+    --num-endings 3 ^
+    --num-main-characters 10 ^
+    --num-main-scenes 10 ^
     --min-num-choices 2 ^
     --max-num-choices 3 ^
     --min-num-choices-opportunity 1 ^
-    --max-num-choices-opportunity 2 ^
-    --enable-image-generation ^
-    --themes "adventure" ^
-    --themes "high-fantasy" ^
-    --themes "science fiction"
+    --max-num-choices-opportunity 3 ^
+    --themes "romance" ^
+    --themes "comedy" ^
+    --themes "drama" ^
+    --enable-image-generation
